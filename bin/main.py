44059af91c5ce07de22ebaa470bb6842d5626626
FilipUniBe <filip.kraehenbuehl@unibe.students.ch>
2023-11-14 21:52:33 +0100

    moved parameters to config-file, implemented grid-search

--- bin/main.py
+++ bin/main.py
@@ -17,6 +17,8 @@ import pymia.evaluation.writer as writer
 
 import joblib
 from sklearn.model_selection import GridSearchCV
+import configparser
+import logging
 
 if sys.version_info[0:2] > (3, 9):
     raise Exception('Python version above 3.9 may cause problems with SimpleITK. [BufferError: memoryview has 1 exported buffer]')
@@ -38,11 +40,22 @@ LOADING_KEYS = [structure.BrainImageTypes.T1w,
                 structure.BrainImageTypes.BrainMask,
                 structure.BrainImageTypes.RegistrationTransform]  # the list of data we will load
 
-# quickening the programm for debugging
-CUT_DATA=True
-CUTOFF_NUMBER=1#3
-USE_JOBLIB=True
-WEIGHTED_DICE=True
+timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+logging.basicConfig(filename=f'script_log_{timestamp}.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+def read_config(config_path):
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    current_directory = os.getcwd()
+    logger.info(f"Current Working Directory: {current_directory}")
+    logger.debug("Sections in the configuration file: %s", config.sections())
+    if 'main' not in config:
+        logger.error("The 'main' section is missing in the configuration file.")
+        raise ValueError("The 'main' section is missing in the configuration file.")
+
+    return config['main']
+
+config = read_config("./config.ini")
 
 # Define the parameter grid
 param_grid = {
@@ -57,8 +70,8 @@ label_mapping = {
             3: 'Hippocampus',
             4: 'Amygdala',
             5: 'Thalamus'
-
         }
+
 
 def get_dice_scores_for_all_labels(results):
     unique_labels = set(
@@ -107,17 +120,18 @@ def calculate_weighted_dice_score_per_label(dice_scores_per_label, volume_per_la
     if len(dice_scores_per_label) != len(dice_scores_per_label):
         raise ValueError("Lengths of dice_scores and voxel_counts must be the same.")
 
+    # Calculate the total voxel count
+    total_voxel_count = sum(volume_per_label.values())
+
     if list(dice_scores_per_label.keys()) != list(volume_per_label.keys()):
         voxel_counts_ordered = order_dicts(dice_scores_per_label, volume_per_label)
 
-        # Calculate the total voxel count
-        total_voxel_count = sum(voxel_counts_ordered.values())
 
-        # Calculate the weighted Dice scores per label
-        #weighted_dice_scores = [(1 - voxel_counts_ordered[label] / total_voxel_count) for label in dice_scores_per_label.keys()]
-        weighted_dice_scores = {label: (1 - voxel_counts_ordered[label] / total_voxel_count) for label in
-                                dice_scores_per_label.keys()}
-        return weighted_dice_scores
+
+    # Calculate the weighted Dice scores per label
+    weighted_dice_scores = {label: (1 - voxel_counts_ordered[label] / total_voxel_count)*dice_scores_per_label[label] for label in
+                            dice_scores_per_label.keys()}
+    return weighted_dice_scores
 
 def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_dir: str, label: int = 0):
     """Brain tissue segmentation using decision forests.
@@ -134,50 +148,96 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
         - Evaluation of the segmentation
     """
 
+    # Cutting of data for faster testing
+    CUT_DATA = config.getboolean('CUT_DATA')
+    CUTOFF_NUMBER = config.getint('CUTOFF_NUMBER')  # 3
+
+    # use joblib
+    USE_JOBLIB_FOREST = config.getboolean('USE_JOBLIB_FOREST')
+    USE_JOBLIB_PREPROCESSING = config.getboolean('USE_JOBLIB_PREPROCESSING')
+
+    USE_GRIDSEARCH = config.getboolean('USE_GRIDSEARCH')
+
+    # use weighted dice score
+    WEIGHTED_DICE = config.getboolean("WEIGHTED_DICE")
+
     # load atlas images
-    total_voxels_avg=putil.load_atlas_images(data_atlas_dir)
+    putil.load_atlas_images(data_atlas_dir)
 
     print('-' * 5, 'Training...')
+    logger.info('------Training...')
+
 
     # crawl the training image directories
     crawler = futil.FileSystemDataCrawler(data_train_dir,
                                           LOADING_KEYS,
                                           futil.BrainImageFilePathGenerator(),
                                           futil.DataDirectoryFilter())
+    logger.info('Crawler was run')
     pre_process_params = {'skullstrip_pre': True,
                           'normalization_pre': True,
                           'registration_pre': True,
                           'coordinates_feature': True,
                           'intensity_feature': True,
                           'gradient_intensity_feature': True}
+    logger.info('pre_process_params were set')
 
-    if not USE_JOBLIB:
+    if not USE_JOBLIB_FOREST:
 
-        images = putil.pre_process_batch(crawler.data, pre_process_params, multi_process=False, label=label)
+        if not USE_JOBLIB_PREPROCESSING:
+            images = putil.pre_process_batch(crawler.data, pre_process_params, multi_process=False, label=label)
+            joblib.dump(images, './bin/preprocessed_images.joblib')
+            logger.info('preprocessed_images were dumped to joblib')
+
+        if USE_JOBLIB_PREPROCESSING:
+            images = joblib.load('./bin/preprocessed_images.joblib')
+            print('[INFO] Loaded preprocessed images from joblib')
+            logger.info('[INFO] Loaded preprocessed images from joblib')
 
         # generate feature matrix and label vector
         data_train = np.concatenate([img.feature_matrix[0] for img in images])
         labels_train = np.concatenate([img.feature_matrix[1] for img in images]).squeeze()
+        logger.info('data_train and labels_train were generated')
 
-        n_estimators = 100  # 100
-        max_depth = 40  # 40
+        # Create the Random Forest model
 
-        forest = sk_ensemble.RandomForestClassifier(max_features=images[0].feature_matrix[0].shape[1],
-                                                    n_estimators=n_estimators,
-                                                    max_depth=max_depth)
+        if USE_GRIDSEARCH:
+            initforest = sk_ensemble.RandomForestClassifier(max_features=images[0].feature_matrix[0].shape[1])
+            start_time = timeit.default_timer()
+            print('[INFO] Start grid search (this may take some time)')
+            logger.info('[INFO] Start grid search (this may take some time)')
+            grid_search = GridSearchCV(initforest, param_grid=param_grid, cv=3, scoring='accuracy')
+            model_grid = grid_search.fit(data_train, labels_train)
+            print(' Time elapsed:', timeit.default_timer() - start_time, 's')
+
+            # Get the best hyperparameters
+            best_params = model_grid.best_params_
+            best_params['max_features']= images[0].feature_matrix[0].shape[1]
+            print("Best Hyperparameters:", best_params)
+            logger.info("Best Hyperparameters:", best_params)
+
+            # Train the model with the best hyperparameters
+            forest = sk_ensemble.RandomForestClassifier(**model_grid.best_params_)
+        else:
+            N_ESTIMATORS= config.getint('N_ESTIMATORS')
+            MAX_DEPTH= config.getint('MAX_DEPTH')
+            forest = sk_ensemble.RandomForestClassifier(max_features=images[0].feature_matrix[0].shape[1], n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH)
 
         start_time = timeit.default_timer()
+        print('[INFO] Start training (this may take some time)')
+        logger.info('[INFO] Start training (this may take some time)')
         forest.fit(data_train, labels_train)
         print(' Time elapsed:', timeit.default_timer() - start_time, 's')
 
-        joblib.dump(forest, 'forest.pkl')
+        joblib.dump(forest, './bin/forest.pkl')
+
     else:
         try:
-            forest = joblib.load('forest.pkl')
+            forest = joblib.load('./bin/forest.pkl')
             print('[INFO] Use old forest.pkl')
         except:
             print('[ERROR] No pickle file found, cancelling program')
-            print('[ERROR] Please set "USE_JOBLIB=False" before next run')
+            print('[ERROR] Please set "USE_JOBLIB_FOREST=False" before next run')
             exit(1)
 
     # create a result directory with timestamp
@@ -186,12 +246,15 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
     os.makedirs(result_dir, exist_ok=True)
 
     print('-' * 5, 'Testing...')
+    logger.info('------Testing...')
 
     # initialize evaluator
     if binary_classification:
         evaluator = putil.init_evaluator(label)
+        print('[INFO] Binary classification')
     else:
         evaluator = putil.init_evaluator()
+        print('[INFO] Multi-label classification')
 
     # crawl the training image directories
     crawler = futil.FileSystemDataCrawler(data_test_dir,
@@ -215,6 +278,7 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
 
     for img in images_test:
         print('-' * 10, 'Testing', img.id_)
+        logger.info('-----------Testing... img')
 
         start_time = timeit.default_timer()
         predictions = forest.predict(img.feature_matrix[0])
@@ -245,11 +309,27 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
         # save results
         sitk.WriteImage(images_prediction[i], os.path.join(result_dir, images_test[i].id_ + '_SEG.mha'), True)
 
-
     # use two writers to report the results
     os.makedirs(result_dir, exist_ok=True)  # generate result directory, if it does not exists
     result_file = os.path.join(result_dir, 'results.csv')
     writer.CSVWriter(result_file).write(evaluator.results)
+
+    # Access some parameters (get either from joblib or from freshly trained model)
+    best_params={'n_estimators': forest.n_estimators, 'max_depth': forest.max_depth, 'max_features': forest.max_features}
+    print("Best Hyperparameters:\n")
+    for key, value in best_params.items():
+        print(f"{key}: {value}\n")
+
+    # write run info file
+    runinfofile = os.path.join(result_dir, 'RunInfo.txt')
+    with open(runinfofile, 'w') as f:
+        f.write("General info:\n")
+
+        f.write("binary_classification: " + str(binary_classification) + "\n")
+        f.write("label: "+str(label)+"\n")
+        f.write("\nBest Hyperparameters:\n")
+        for key, value in best_params.items():
+            f.write(f'{key}: {value}\n')
 
     if WEIGHTED_DICE:
         # get dice scores for all labels
@@ -257,7 +337,6 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
 
         # Remove the second entry [1] from each list (for some reason I get double entries for each label)
         dice_scores_all_labels = {label: score[0] for label, score in dice_scores_all_labels.items()}
-
 
         #Get voxel count per label
         volume_per_img = [map_fields(get_voxel_count_per_label(img),label_mapping) for img in images_prediction]
@@ -273,21 +352,17 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
         # Calculate the mean voxel count per label
         mean_voxel_count_per_label = {label: np.mean(volumes) for label, volumes in voxel_count_per_label.items()}
 
-        #calculate ratio from atlas
-        #ratio_voxel_count_per_label = {label: volume / total_voxels_avg for label, volume in mean_voxel_count_per_label.items()}
         del mean_voxel_count_per_label["Background"]#remove background #fixme background same as total_voxels_avg???
 
         weighted_dice_scores = calculate_weighted_dice_score_per_label(dice_scores_all_labels, mean_voxel_count_per_label)
 
-
         print("Weighted Dice Scores per Label:")
         for label, score in weighted_dice_scores.items():
             print(f"Label {label}: {score}")
-        weighted_dice_file = os.path.join(result_dir, 'weighted_dice.txt')
-        with open(weighted_dice_file, 'w') as f:
+        with open(runinfofile, 'a') as f:
+            f.write("\nWeighted Dice Scores:\n")
             for label, score in weighted_dice_scores.items():
                 f.write(f"Label {label}: {score}\n")
-
 
     print('\nSubject-wise results...')
     writer.ConsoleWriter(use_logging=True).write(evaluator.results)
@@ -302,17 +377,13 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
     # clear results such that the evaluator is ready for the next evaluation
     evaluator.clear()
 
-# extract dice scores for all labels
-
-
 if __name__ == "__main__":
     """The program's entry point."""
     ##TODO: use binary classification
 
-    binary_classification = False
-    label = 5  # 1: "WhiteMatter",        2: "GreyMatter",        3: "Hippocampus",        4: "Amygdala",        5: "Thalamus",
 
-
+    binary_classification = config.getboolean('binary_classification')
+    label = config.getint('label')
 
     script_dir = os.path.dirname(sys.argv[0])
 
